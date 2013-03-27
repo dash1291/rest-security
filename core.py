5e40fd0fcc296249359184273a609799a4b914df
Ashish Dubey <ashish.dubey91@gmail.com>
2013-03-27 19:44:41 +0530

    write to_request() and from_request()

--- core.py
+++ core.py
@@ -20,24 +20,25 @@ class Message(object):
         self.msg_encypt_algo = kwargs['msg_encypt_algo']
         self.signature_algo = kwargs['signature_algo']
 
-        self.headers = kwargs['headers']
         self.payload = kwargs['payload']
 
+        # If the values were not specified, we calculate them.
+        # Generally, we don't specify these values for outgoing messages,
+        # while they'll be present in incoming messages.
         if 'digest_val' in kwargs:
             self.digest_val = kwargs['digest_val']
         else:
-            self.digest_val = self._calculate_digest(params)
+            self.digest_val = self._calculate_digest()
 
         if 'signature_val' in kwargs:
             self.signature_val = kwargs['signature_val']
         else:
-            self.signature_val = self._calculate_signature(params)
+            self.signature_val = self._calculate_signature()
 
         if 'key_encrypted' in kwargs:
-            self.signature_val = kwargs['key_encrypted']
+            self.key_encrypted = kwargs['key_encrypted']
         else:
-            self.key_encrypted = self._calculate_sym_key(params)
-
+            self.key_encrypted = self._calculate_sym_key()
 
     """
     Verifies if the signature received matches the calculated one.
@@ -45,39 +46,69 @@ class Message(object):
     Raises an exception, when `signature` is not found as an object variable.
     """
     def verify_signature(self):
-        # TODO
+        if self.signature_val == self._calculate_signature():
+            return True
+        else:
+            return False
 
     def _calculate_signature(self, **kwargs):
         # TODO
+        return signature_val
 
     def _calculate_sym_key(self, **kwargs):
         # TODO
+        return symmetric_key
 
     def _calculate_digest(self, **kwargs):
         # TODO
+        return digest_val
 
     """
-    Build and return a headers dict for the message.
+    Build and return the message headers dict and payload tuple.
     """
-    def to_headers(self):
-        return ''
+    def to_request(self):
+        prefix = 'X-JAG-'
+        headers = {}
+        headers[prefix + 'CertificateId'] = self.certificate_id
+        headers[prefix + 'DigestAlg'] = self.digest_alg
+        headers[prefix + 'DigestValue'] = self.digest_val
+        headers[prefix + 'SigAlg'] = self.signature_algo
+        headers[prefix + 'SigValue'] = self.signature_val
+        headers[prefix + 'EncAlg'] = self.msg_encypt_algo
+        headers[prefix + 'EncKeyAlg'] = self.key_encrypt_algo
+        headers[prefix + 'EncKeyValue'] = self.key_encrypted
+
+        payload = self.payload
+
+        return (headers, payload)
 
     """
-    Create a `Message` instance from headers supplied.
+    Create a `Message` instance from headers dict and payload supplied.
     """
     @staticmethod
-    def from_headers(headers_dict):
-        return Message()
+    def from_request(headers_dict, payload):
+        prefix = 'X-JAG-'
 
+        return Message(certificate_id=headers_dict[prefix + 'CertificateId'],
+            digest_algo = headers_dict[prefix + 'DigestAlg'],
+            digest_val = headers_dict[prefix + 'DigestValue'],
+            signature_algo = headers_dict[prefix + 'SigAlg'],
+            signature_val = headers_dict[prefix + 'SigValue'],
+            msg_encrypt_algo = headers_dict[prefix + 'EncAlg'],
+            key_encrypt_algo = headers_dict[prefix + 'EncKeyAlg'],
+            key_encrypted = headers_dict[prefix + 'EncKeyValue'],
+            payload=payload)
 
 """
 Encrypts a message.
 """
-def encrypt(self):
-    # encrypt the message and return a dict
+def encrypt(message):
+    # encrypt the message
+    return Message()
 
 """
 Utility method to decrypt.
 """
-def decrypt(message_dict):
+def decrypt(message):
     # return Message object after decryption
+    return Message()
